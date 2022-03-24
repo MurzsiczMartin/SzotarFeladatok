90426daf3924cf0939526bc7e469d82da3bc8905
pp <pp@gszi.edu.hu>
2022-03-24 19:11:06 +0100

    Szotar txt hozzáadása és pár feladat

--- Szotar/Program.cs
+++ Szotar/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 
 namespace Szotar
 {
@@ -7,7 +7,31 @@ namespace Szotar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string fajlnev = @"..\..\..\szotar.txt";
+                string[] sorok = File.ReadAllLines(fajlnev);
+                Console.WriteLine("A fájl sorainak száma: {0}", sorok.Length);
+
+                Console.WriteLine("Adjon meg egy kezdőbetűt!");
+                string kb = Console.ReadLine();
+
+                foreach (string szo in sorok)
+                {
+                    if (szo.StartsWith(kb))
+                    {
+                        Console.WriteLine(szo);
+                    }
+                }
+
+                //a megfelelő sorszámú szó kiírása
+            }
+            catch (Exception hiba)
+            {
+                Console.WriteLine("Sajnos hiba történt a fájl megnyitása közben!");
+                Console.WriteLine(hiba.Message); ;
+            }
+               
         }
     }
 }
