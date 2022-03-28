7d02fe85db6a954118374d615b9a34009b9cee2a
pp <pp@gszi.edu.hu>
2022-03-28 17:15:41 +0200

    Valasztasok project hozzáadása

--- Szotar/Program.cs
+++ Szotar/Program.cs
@@ -25,11 +25,34 @@ namespace Szotar
                 }
 
                 //a megfelelő sorszámú szó kiírása
+
+                Console.WriteLine("Kérem egy szó sorszámát!");
+                int sor = int.Parse(Console.ReadLine());
+                //int sor2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A {0}. szó: {1}",sor,sorok[sor-1]);
+
+                //A leghosszabb szavak megkeresése és listázása
+                int maxhossz = 0;
+                foreach (var item in sorok)
+                {
+                    if (item.Length > maxhossz) maxhossz = item.Length;
+                }
+                //listázás
+                Console.WriteLine( $"A leghosszabb szavak ({maxhossz}):" );
+                foreach (var item in sorok)
+                {
+                    if (item.Length == maxhossz) Console.WriteLine(item);
+                }
+
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine("Az egész szám formátuma hibás!");
             }
             catch (Exception hiba)
             {
                 Console.WriteLine("Sajnos hiba történt a fájl megnyitása közben!");
-                Console.WriteLine(hiba.Message); ;
+                Console.WriteLine(hiba); ;
             }
                
         }
