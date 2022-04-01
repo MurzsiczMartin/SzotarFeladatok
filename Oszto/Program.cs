4dc6e06c8d9639f6c2c616bdc32f37262af498f3
pp <pp@gszi.edu.hu>
2022-04-01 19:15:21 +0200

    lap és csomag félkész

---
+++ Oszto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Oszto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Csomag pakli = new Csomag();
+            pakli.kever();
+        }
+    }
+}
